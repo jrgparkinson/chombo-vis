332783caedda6aa1d17d9b4d957286b2c6d50067
jparkinson <jamie.parkinson@gmail.com>
2020-04-18 04:33:14 +0100

    Set Terser parallel option explicitly to try and avoid errors compiling on web server

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -29,7 +29,7 @@ module.exports = {
     //   new OptimizeCSSAssetsPlugin({})
     // ]
     minimize: true,
-    minimizer: [new TerserPlugin(),
+    minimizer: [new TerserPlugin({ parallel: 1 }),
                 new OptimizeCSSAssetsPlugin({})]
   },
   module: {
